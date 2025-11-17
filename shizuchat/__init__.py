6e4254eb8bae489bb861d70078a25569c301be86
sidhubad143 <lakhwinder4332@gmail.com>
2025-11-17 17:17:45 +0530

    Update __init__.py

--- shizuchat/__init__.py
+++ shizuchat/__init__.py
@@ -1,43 +1,14 @@
-import logging
-import time
-import pytz
-from pymongo import MongoClient
-from Abg import patch
-from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
+# client.py
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 import config
-import uvloop
 
-uvloop.install()
-
-logging.basicConfig(
-    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
-    datefmt="%d-%b-%y %H:%M:%S",
-    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
-    level=logging.INFO,
-)
-
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-LOGGER = logging.getLogger(__name__)
-boot = time.time()
-mongodb = MongoCli(config.MONGO_URL)
-db = mongodb.Anonymous
-mongo = MongoClient(config.MONGO_URL)
-OWNER = config.OWNER_ID
-
-#time zone
-TIME_ZONE = pytz.timezone(config.TIME_ZONE)
-scheduler = AsyncIOScheduler(timezone=TIME_ZONE)
-
-class shizuchat(Client):
+class ShizuChat(Client):
     def __init__(self):
         super().__init__(
             name="shizuchat",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            lang_code="en",
             bot_token=config.BOT_TOKEN,
             in_memory=True,
             parse_mode=ParseMode.DEFAULT,
@@ -46,13 +17,11 @@ class shizuchat(Client):
     async def start(self):
         await super().start()
         self.id = self.me.id
-        self.name = self.me.first_name + " " + (self.me.last_name or "")
+        self.name = (self.me.first_name or "") + " " + (self.me.last_name or "")
         self.username = self.me.username
         self.mention = self.me.mention
 
     async def stop(self):
         await super().stop()
 
-
-shizuchat = shizuchat()
-    
+shizuchat = ShizuChat()
