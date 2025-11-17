68ec38005f2a26836572bfb53064c3c621c1c663
sidhubad143 <lakhwinder4332@gmail.com>
2025-11-17 17:18:08 +0530

    Update __main__.py

--- shizuchat/__main__.py
+++ shizuchat/__main__.py
@@ -1,65 +1,65 @@
+# main.py
+import uvloop
+uvloop.install()
+
 import sys
 import asyncio
 import importlib
-from flask import Flask
 import threading
+
 from pyrogram import idle
 from pyrogram.types import BotCommand
+
+from logger import LOGGER
+from client import shizuchat
+from database import mongodb
+from scheduler import scheduler
+from server import run_flask
 from config import OWNER_ID
-from shizuchat import LOGGER, shizuchat
 from shizuchat.modules import ALL_MODULES
 
 
-async def anony_boot():
+async def boot():
     try:
         await shizuchat.start()
     except Exception as ex:
-        LOGGER.error(ex)
+        LOGGER.error(f"Start Error: {ex}")
         sys.exit(1)
 
-    for all_module in ALL_MODULES:
-        importlib.import_module("shizuchat.modules." + all_module)
-        LOGGER.info(f"ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ɪᴍᴘᴏʀᴛᴇᴅ: {all_module}")
+    # import modules
+    for module in ALL_MODULES:
+        importlib.import_module("shizuchat.modules." + module)
+        LOGGER.info(f"Imported module: {module}")
 
-    # Set bot commands
+    # set bot commands
     try:
         await shizuchat.set_bot_commands(
-            commands=[
-                BotCommand("start", "✧ sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ ✧"),
-                BotCommand("help", "✧ ɢᴇᴛ ᴛʜᴇ ʜᴇʟᴘ ᴍᴇɴᴜ ✧"),
-                BotCommand("ping", "✧ ᴄʜᴇᴄᴋ ɪғ ᴛʜᴇ ʙᴏᴛ ɪs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇsᴅ ✧"),
-                BotCommand("shipping", "✧ ᴄᴏᴜᴘʟᴇs ᴏғ ᴅᴀʏ ✧"),
-                BotCommand("rankings", "✧ ᴜsᴇʀ ᴍsɢ ʟᴇᴀᴅᴇʀʙᴏᴀʀᴅ ✧"),
+            [
+                BotCommand("start", "Start the bot"),
+                BotCommand("help", "Help menu"),
+                BotCommand("ping", "Check bot status"),
+                BotCommand("shipping", "Couples of the Day"),
+                BotCommand("rankings", "User msg leaderboard"),
             ]
         )
-        LOGGER.info("ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅꜱ ꜱᴇᴛ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ.")
+        LOGGER.info("Bot commands set.")
     except Exception as ex:
-        LOGGER.error(f"ꜰᴀɪʟᴇᴅ ᴛᴏ ꜱᴇᴛ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅꜱ: {ex}")
+        LOGGER.error(f"Bot command error: {ex}")
 
-    LOGGER.info(f"@{shizuchat.username} Started.")
+    # Notify owner
     try:
-        await shizuchat.send_message(int(OWNER_ID), f"{shizuchat.mention} has started")
-    except Exception as ex:
-        LOGGER.info(f"@{shizuchat.username} ꜱᴛᴀʀᴛᴇᴅ, ᴘʟᴇᴀꜱᴇ ꜱᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ ꜰʀᴏᴍ ᴏᴡɴᴇʀ ɪᴅ.")
-    
+        await shizuchat.send_message(OWNER_ID, f"{shizuchat.mention} is now online.")
+    except:
+        LOGGER.info("Bot started, but owner didn't receive the message.")
+
     await idle()
 
 
-# Flask Server Code for Health Check
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Bot is running"
-
-def run_flask():
-    app.run(host="0.0.0.0", port=8000)
-
 if __name__ == "__main__":
-    # Start Flask server in a new thread
+    # start flask in separate thread
     flask_thread = threading.Thread(target=run_flask)
     flask_thread.start()
 
-    # Start the bot asynchronously
-    asyncio.get_event_loop().run_until_complete(anony_boot())
-    LOGGER.info("ꜱᴛᴏᴘᴘɪɴɢ ꜱʜɪᴢᴜᴄʜᴀᴛ ʙᴏᴛ...")
+    # run bot
+    asyncio.get_event_loop().run_until_complete(boot())
+    LOGGER.info("Stopping shizuchat...")
